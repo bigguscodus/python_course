971cc2ede51d28dd4ec7321e204dbce6372f4b11
Tolmachev Mikhail <54994201+bigguscodus@users.noreply.github.com>
2020-11-09 20:58:28 +0300

    HW-1 bigbrain solution for power of 2

--- hw1/sample_project/calculator/calc.py
+++ hw1/sample_project/calculator/calc.py
@@ -2,6 +2,4 @@ import math
 
 
 def check_power_of_2(a: int) -> bool:
-    if a < 0 or a == 0 or a == 1:
-        return False
-    return math.log(a, 2).is_integer()
+    return (a & (a - 1) == 0) and a != 0
