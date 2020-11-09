11fda54fd43354b978900d92d446471024677c61
Tolmachev Mikhail <54994201+bigguscodus@users.noreply.github.com>
2020-11-09 20:42:15 +0300

    HW-1 update function

--- hw1/sample_project/calculator/calc.py
+++ hw1/sample_project/calculator/calc.py
@@ -1,2 +1,7 @@
+import math
+
+
 def check_power_of_2(a: int) -> bool:
-    return not (bool(a & (a - 1)))
+    if a < 0 or a == 0 or a == 1:
+        return False
+    return math.log(a, 2).is_integer()
